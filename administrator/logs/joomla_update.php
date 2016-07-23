08c31b25f1db2197f49651d18e910ffee43a101c
Wilco Alsemgeest <info@con-con.nl>
2016-07-23 08:34:39 +0200

    #85 Update Joomla to 3.6

---
+++ administrator/logs/joomla_update.php
@@ -0,0 +1,30 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2016-07-23 06:28:50 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2016-07-23T06:28:50+00:00	INFO ::1	update	Update gestart door gebruiker conconnl (478). Oude versie is 3.5.1.
+2016-07-23T06:28:51+00:00	INFO ::1	update	Downloaden updatebestand van https://github.com/joomla/joomla-cms/releases/download/3.6.0/Joomla_3.6.0-Stable-Update_Package.zip.
+2016-07-23T06:29:00+00:00	INFO ::1	update	Bestand Joomla_3.6.0-Stable-Update_Package.zip downloaden geslaagd.
+2016-07-23T06:29:46+00:00	INFO ::1	update	Update gestart door gebruiker conconnl (478). Oude versie is 3.5.1.
+2016-07-23T06:29:46+00:00	INFO ::1	update	Bestand Joomla_3.6.0-Stable-Update_Package.zip downloaden geslaagd.
+2016-07-23T06:31:32+00:00	INFO ::1	update	Installatie nieuwe versie is gestart.
+2016-07-23T06:31:35+00:00	INFO ::1	update	Voltooien installatie.
+2016-07-23T06:31:36+00:00	INFO ::1	update	Zoekopdracht uitgevoerd in bestand 3.6.0-2016-04-01. Zoektekst: UPDATE `#__update_sites` SET `name` = 'Joomla! Core' WHERE `name` = 'Joomla Core.
+2016-07-23T06:31:36+00:00	INFO ::1	update	Zoekopdracht uitgevoerd in bestand 3.6.0-2016-04-01. Zoektekst: UPDATE `#__update_sites` SET `name` = 'Joomla! Extension Directory' WHERE `name`.
+2016-07-23T06:31:36+00:00	INFO ::1	update	Zoekopdracht uitgevoerd in bestand 3.6.0-2016-04-01. Zoektekst: UPDATE `#__update_sites` SET `location` = 'https://update.joomla.org/core/list.x.
+2016-07-23T06:31:36+00:00	INFO ::1	update	Zoekopdracht uitgevoerd in bestand 3.6.0-2016-04-01. Zoektekst: UPDATE `#__update_sites` SET `location` = 'https://update.joomla.org/jed/list.xm.
+2016-07-23T06:31:36+00:00	INFO ::1	update	Zoekopdracht uitgevoerd in bestand 3.6.0-2016-04-01. Zoektekst: UPDATE `#__update_sites` SET `location` = 'https://update.joomla.org/language/tr.
+2016-07-23T06:31:36+00:00	INFO ::1	update	Zoekopdracht uitgevoerd in bestand 3.6.0-2016-04-01. Zoektekst: UPDATE `#__update_sites` SET `location` = 'https://update.joomla.org/core/extens.
+2016-07-23T06:31:36+00:00	INFO ::1	update	Zoekopdracht uitgevoerd in bestand 3.6.0-2016-04-06. Zoektekst: ALTER TABLE `#__redirect_links` MODIFY `new_url` VARCHAR(2048);.
+2016-07-23T06:31:36+00:00	INFO ::1	update	Zoekopdracht uitgevoerd in bestand 3.6.0-2016-04-08. Zoektekst: INSERT INTO `#__extensions` (`extension_id`, `name`, `type`, `element`, `folder`.
+2016-07-23T06:31:36+00:00	INFO ::1	update	Zoekopdracht uitgevoerd in bestand 3.6.0-2016-04-08. Zoektekst: UPDATE `#__update_sites_extensions` SET `extension_id` = 802 WHERE `update_site_.
+2016-07-23T06:31:36+00:00	INFO ::1	update	Zoekopdracht uitgevoerd in bestand 3.6.0-2016-04-09. Zoektekst: ALTER TABLE `#__menu_types` ADD COLUMN `asset_id` INT(11) NOT NULL AFTER `id`;.
+2016-07-23T06:31:36+00:00	INFO ::1	update	Zoekopdracht uitgevoerd in bestand 3.6.0-2016-05-06. Zoektekst: DELETE FROM `#__extensions` WHERE `type` = 'library' AND `element` = 'simplepie'.
+2016-07-23T06:31:36+00:00	INFO ::1	update	Zoekopdracht uitgevoerd in bestand 3.6.0-2016-05-06. Zoektekst: INSERT INTO `#__extensions` (`extension_id`, `name`, `type`, `element`, `folder`.
+2016-07-23T06:31:36+00:00	INFO ::1	update	Zoekopdracht uitgevoerd in bestand 3.6.0-2016-06-01. Zoektekst: UPDATE `#__extensions` SET `protected` = 1, `enabled` = 1  WHERE `name` = 'com_a.
+2016-07-23T06:31:36+00:00	INFO ::1	update	Zoekopdracht uitgevoerd in bestand 3.6.0-2016-06-05. Zoektekst: ALTER TABLE `#__languages` ADD COLUMN `asset_id` INT(11) NOT NULL AFTER `lang_id.
+2016-07-23T06:31:36+00:00	INFO ::1	update	Verwijderen van verwijderde bestanden en mappen.
+2016-07-23T06:31:41+00:00	INFO ::1	update	Opschonen na installatie.
+2016-07-23T06:31:41+00:00	INFO ::1	update	Update naar versie 3.6.0 is voltooid.
