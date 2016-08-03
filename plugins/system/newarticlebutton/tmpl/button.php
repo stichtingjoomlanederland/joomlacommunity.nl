5c875d96e0f5657fb68239a07ca8522671c15e18
Niels van der Veer (n9iels) <n.i.vdveer@gmail.com>
2016-08-03 17:34:59 +0200

    #40 Install New Article Button plugin

---
+++ plugins/system/newarticlebutton/tmpl/button.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package     New_Article_Button
+ * @subpackage  plg_system_newarticlebutton
+ *
+ * @copyright   Copyright (C) 2016 Niels van der Veer, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later
+ */
+
+defined('_JEXEC') or die;
+?>
+<div class="newarticlebutton">
+	<a href="<?php echo $editUrl; ?>" class="btn btn-success btn-lg">
+		<?php echo JText::_("PLG_SYSTEM_NEWARTICLEBUTTON_EDIT_CATEGORY"); ?>
+	</a>
+</div>
