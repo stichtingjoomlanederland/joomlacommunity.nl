1e7ee029b8038f826e06f11c41246137c0029f35
Sander Potjer <github@sanderpotjer.nl>
2016-12-03 20:44:43 +0100

    Extensions cleanup

--- administrator/components/com_slider/sql/install.mysql.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS `#__slider` (
-	`id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-	`published` tinyint(4) NOT NULL DEFAULT '1',
-	`title` varchar(250) NOT NULL DEFAULT '',
-	`image` text NOT NULL,
-	`image_alt` text NOT NULL,
-	`text` varchar(250) NOT NULL DEFAULT '',
-	PRIMARY KEY (`id`)
-);
