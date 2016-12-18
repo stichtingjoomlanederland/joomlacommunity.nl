d081a2c8ce8db6d7325634fc61ba80bdfd44fcb4
Sander Potjer <github@sanderpotjer.nl>
2016-12-18 13:06:37 +0100

    #227 Use images homepage news

--- public_html/templates/jc/html/mod_articles_category/latestnews.php
+++ public_html/templates/jc/html/mod_articles_category/latestnews.php
@@ -13,33 +13,28 @@ defined('_JEXEC') or die;
 <h3><?php echo $module->title; ?></h3>
 <div class="list-group list-group-flush <?php echo $moduleclass_sfx; ?>">
 	<?php foreach ($list as $item) : ?>
-		<?php
-		$images = json_decode($item->images);
-		if (isset($images->image_intro) && !empty($images->image_intro) and file_exists($images->image_intro))
-		{
-			$image_intro_alt = $images->image_intro_alt ? $images->image_intro_alt : $item->title;
-			echo JHtml::_('image', $images->image_intro, $image_intro_alt, array('class' => 'news-image'));
-		}
-		?>
-		<a class="list-group-item" href="<?php echo $item->link; ?>">
-			<div class="news-image">
-				<img src="http://placehold.it/150x150"/>
-			</div>
-			<div class="date-icon">
-				<span class="date-day"><?php echo JHtml::_('date', $item->publish_up, JText::_('j')); ?></span><?php echo JHtml::_('date', $item->publish_up, JText::_('M')); ?>
-			</div>
-			<h4 class="list-group-item-heading">
+		<?php $images = json_decode($item->images); ?>
+        <a class="list-group-item" href="<?php echo $item->link; ?>">
+			<?php if (isset($images->image_intro) && !empty($images->image_intro) and file_exists($images->image_intro)): ?>
+                <div class="news-image">
+                    <img src="<?php echo $images->image_intro; ?>"/>
+                </div>
+			<?php endif; ?>
+            <div class="date-icon">
+                <span class="date-day"><?php echo JHtml::_('date', $item->publish_up, JText::_('j')); ?></span><?php echo JHtml::_('date', $item->publish_up, JText::_('M')); ?>
+            </div>
+            <h4 class="list-group-item-heading">
 				<?php echo $item->title; ?>
-			</h4>
-			<p class="list-group-item-text">
+            </h4>
+            <p class="list-group-item-text">
 				<?php echo strip_tags($item->displayIntrotext); ?>
-			</p>
-			<small>
-				<i class="fa fa-folder" aria-hidden="true"></i> <?php echo $item->category_title; ?>&nbsp;&nbsp;<i class="fa fa-user" aria-hidden="true"></i> <?php echo $item->displayAuthorName; ?>
-			</small>
-		</a>
+            </p>
+            <small>
+                <i class="fa fa-folder" aria-hidden="true"></i> <?php echo $item->category_title; ?>&nbsp;&nbsp;<i class="fa fa-user" aria-hidden="true"></i> <?php echo $item->displayAuthorName; ?>
+            </small>
+        </a>
 	<?php endforeach; ?>
 </div>
 <a href="<?php echo JRoute::_('index.php?Itemid=240'); ?>" class="btn btn-nieuws btn-block">
-	Meer Joomla-nieuws
+    Meer Joomla-nieuws
 </a>
