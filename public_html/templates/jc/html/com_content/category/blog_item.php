353a4265d36088c74a34d2fc46b4d175fde700e1
Sander Potjer <github@sanderpotjer.nl>
2016-12-19 23:49:19 +0100

    Linkedin share blog item

--- public_html/templates/jc/html/com_content/category/blog_item.php
+++ public_html/templates/jc/html/com_content/category/blog_item.php
@@ -38,118 +38,118 @@ else
 $showArticleInformation = ($params->get('show_create_date') || $params->get('show_category') || $params->get('show_author'));
 
 ?>
-<div class="well <?php echo ($image == 'large' ? 'photoheader' : ''); ?>">
+<div class="well <?php echo($image == 'large' ? 'photoheader' : ''); ?>">
 	<?php if ($image == 'large'): ?>
-		<div class="photobox">
-			<img src="<?php echo($images->image_fulltext); ?>"/>
-		</div>
+        <div class="photobox">
+            <img src="<?php echo($images->image_fulltext); ?>"/>
+        </div>
 	<?php endif; ?>
-	<div class="row">
-		<div class="col-2">
+    <div class="row">
+        <div class="col-2">
 			<?php if ($image == 'small'): ?>
-				<div class="photoboxsmall<?php if ($images->float_intro == 'right'): ?> logo<?php endif; ?>">
-					<img src="<?php echo($images->image_intro); ?>"/>
-				</div>
+                <div class="photoboxsmall<?php if ($images->float_intro == 'right'): ?> logo<?php endif; ?>">
+                    <img src="<?php echo($images->image_intro); ?>"/>
+                </div>
 			<?php endif; ?>
 
-			<div class="item-meta">
+            <div class="item-meta">
 				<?php if ($showArticleInformation != false) : ?>
 					<?php if ($params->get('show_author')) : ?>
-						<div class="auteur-info">
+                        <div class="auteur-info">
 							<?php
 							if (!empty($this->item->created_by_alias)) : ?>
-								<strong>Door</strong>
-								<p>
+                                <strong>Door</strong>
+                                <p>
 									<?php echo $this->item->created_by_alias; ?>
-								</p>
+                                </p>
 							<?php else: ?>
-								<p>
-									<a href="<?php echo $profile->getLink(); ?>">
-										<img class="img-circle" width="80px" src="<?php echo $profile->getAvatar(); ?>"/>
-									</a>
-								</p>
-								<p>
+                                <p>
+                                    <a href="<?php echo $profile->getLink(); ?>">
+                                        <img class="img-circle" width="80px" src="<?php echo $profile->getAvatar(); ?>"/>
+                                    </a>
+                                </p>
+                                <p>
 									<?php echo JHtml::_('link', $profile->getLink(), $profile->user->get('name')); ?>
-								</p>
+                                </p>
 							<?php endif; ?>
-						</div>
+                        </div>
 					<?php endif; ?>
 
 					<?php if ($params->get('show_create_date')) : ?>
-						<div class="item-datum">
-							<strong>Datum</strong>
-							<p>
-								<time class="post-date"><?php echo JHtml::_('date', $this->item->created, JText::_('j F Y')); ?></time>
-							</p>
-						</div>
+                        <div class="item-datum">
+                            <strong>Datum</strong>
+                            <p>
+                                <time class="post-date"><?php echo JHtml::_('date', $this->item->created, JText::_('j F Y')); ?></time>
+                            </p>
+                        </div>
 					<?php endif; ?>
 
 					<?php if ($params->get('show_category')) : ?>
-						<div class="item-categorie">
-							<strong>Categorie</strong>
-							<p>
+                        <div class="item-categorie">
+                            <strong>Categorie</strong>
+                            <p>
 								<?php if ($params->get('link_category') && $this->item->catslug) : ?>
-									<a href="<?php echo JRoute::_(ContentHelperRoute::getCategoryRoute($this->item->catslug)); ?>">
+                                    <a href="<?php echo JRoute::_(ContentHelperRoute::getCategoryRoute($this->item->catslug)); ?>">
 										<?php echo $this->escape($this->item->category_title); ?>
-									</a>
+                                    </a>
 								<?php else : ?>
 									<?php echo $this->escape($this->item->category_title); ?>
 								<?php endif; ?>
-							</p>
-						</div>
+                            </p>
+                        </div>
 					<?php endif; ?>
 				<?php endif; ?>
 
-				<div class="item-share full">
+                <div class="item-share full">
 					<?php
 					$data = array(
-						'title'      => 'Share',
-						'facebook'   => true,
-						'twitter'    => true,
-						'googleplus' => true,
-						'item'       => $this->item
+						'title'    => 'Share',
+						'facebook' => true,
+						'twitter'  => true,
+						'linkedin' => true,
+						'item'     => $this->item
 					);
 					echo JLayoutHelper::render('template.snippet-share-page', $data);
 					?>
-				</div>
-			</div>
-		</div>
+                </div>
+            </div>
+        </div>
 
-		<div class="col-md-9">
-			<div class="item">
-				<div class="page-header">
+        <div class="col-md-9">
+            <div class="item">
+                <div class="page-header">
 					<?php if ($canEdit) : ?>
-						<div class="edit-buttons">
+                        <div class="edit-buttons">
 							<?php echo JLayoutHelper::render('joomla.content.icons', array('params' => $params, 'item' => $this->item, 'print' => false)); ?>
-						</div>
+                        </div>
 					<?php endif; ?>
 
 					<?php if ($params->get('show_title')) : ?>
-						<h2>
+                        <h2>
 							<?php if ($params->get('link_titles') && $params->get('access-view')) : ?>
-								<a href="<?php echo JRoute::_(ContentHelperRoute::getArticleRoute($this->item->slug, $this->item->catid)); ?>">
+                                <a href="<?php echo JRoute::_(ContentHelperRoute::getArticleRoute($this->item->slug, $this->item->catid)); ?>">
 									<?php echo $this->escape($this->item->title); ?>
-								</a>
+                                </a>
 							<?php else : ?>
 								<?php echo $this->escape($this->item->title); ?>
 							<?php endif; ?>
-						</h2>
+                        </h2>
 					<?php endif; ?>
 
 					<?php if ($this->item->state == 0) : ?>
-						<span class="label label-warning"><?php echo JText::_('JUNPUBLISHED'); ?></span>
+                        <span class="label label-warning"><?php echo JText::_('JUNPUBLISHED'); ?></span>
 					<?php endif; ?>
-				</div>
+                </div>
 
-				<div class="item-content">
+                <div class="item-content">
 					<?php echo $this->item->introtext; ?>
-				</div>
+                </div>
 
 				<?php if ($params->get('show_readmore') && $this->item->readmore) :
 					$link = JRoute::_(ContentHelperRoute::getArticleRoute($this->item->slug, $this->item->catid));
 					?>
 
-					<a class="btn btn-nieuws" href="<?php echo $link; ?>">
+                    <a class="btn btn-nieuws" href="<?php echo $link; ?>">
 
 						<?php if ($readmore = $this->item->alternative_readmore) :
 							echo $readmore;
@@ -163,10 +163,10 @@ $showArticleInformation = ($params->get('show_create_date') || $params->get('sho
 							echo JHtml::_('string.truncate', ($this->item->title), $params->get('readmore_limit'));
 						endif; ?>
 
-					</a>
+                    </a>
 
 				<?php endif; ?>
-			</div>
-		</div>
-	</div>
+            </div>
+        </div>
+    </div>
 </div>
