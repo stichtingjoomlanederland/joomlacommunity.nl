019802b3ab02cd41f3897aa69b9f49503bdc2c18
Sander Potjer <github@sanderpotjer.nl>
2016-12-20 22:02:44 +0100

    Homepage news update

--- public_html/templates/jc/html/mod_articles_category/latestnews.php
+++ public_html/templates/jc/html/mod_articles_category/latestnews.php
@@ -20,18 +20,15 @@ defined('_JEXEC') or die;
                     <img src="<?php echo $images->image_intro; ?>"/>
                 </div>
 			<?php endif; ?>
-            <div class="date-icon">
-                <span class="date-day"><?php echo JHtml::_('date', $item->publish_up, JText::_('j')); ?></span><?php echo JHtml::_('date', $item->publish_up, JText::_('M')); ?>
-            </div>
+            <p class="list-group-item-meta">
+                <strong><?php echo JHtml::_('date', $item->publish_up, JText::_('j M Y')); ?></strong>, door <?php echo $item->displayAuthorName; ?>
+            </p>
             <h4 class="list-group-item-heading">
 				<?php echo $item->title; ?>
             </h4>
             <p class="list-group-item-text">
 				<?php echo strip_tags($item->displayIntrotext); ?>
             </p>
-            <small>
-                <i class="fa fa-folder" aria-hidden="true"></i> <?php echo $item->category_title; ?>&nbsp;&nbsp;<i class="fa fa-user" aria-hidden="true"></i> <?php echo $item->displayAuthorName; ?>
-            </small>
         </a>
 	<?php endforeach; ?>
 </div>
