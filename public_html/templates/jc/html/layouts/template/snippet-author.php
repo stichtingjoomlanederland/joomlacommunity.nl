69537787b9d84a95418167a8069aed99c20c7b2b
Sander Potjer <github@sanderpotjer.nl>
2016-12-20 22:58:11 +0100

    Fix article author sharing icons

--- public_html/templates/jc/html/layouts/template/snippet-author.php
+++ public_html/templates/jc/html/layouts/template/snippet-author.php
@@ -26,22 +26,22 @@ $profile->linkedin = $userparams->get('linkedin', '');
 		<ul class="list-inline share-buttons">
 			<?php if ($profile->twitter): ?>
 				<li class="share-twitter">
-					<a href="<?php echo($profile->twitter); ?>" target="_blank"><span class="icon icon-twitter"></span></a>
+					<a href="<?php echo($profile->twitter); ?>" target="_blank"><i class="fa fa-twitter-square" aria-hidden="true"></i></a>
 				</li>
 			<?php endif; ?>
 			<?php if ($profile->facebook): ?>
 				<li class="share-facebook">
-					<a href="<?php echo($profile->facebook); ?>" target="_blank"><span class="icon icon-facebook"></span></a>
+					<a href="<?php echo($profile->facebook); ?>" target="_blank"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
 				</li>
 			<?php endif; ?>
 			<?php if ($profile->linkedin): ?>
 				<li class="share-linkedin">
-					<a href="<?php echo($profile->linkedin); ?>" target="_blank"><span class="icon icon-linkedin"></span></a>
+					<a href="<?php echo($profile->linkedin); ?>" target="_blank"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
 				</li>
 			<?php endif; ?>
 			<?php if ($profile->website): ?>
 				<li class="share-website">
-					<a href="<?php echo($profile->website); ?>" target="_blank"><span class="icon icon-website"></span></a>
+					<a href="<?php echo($profile->website); ?>" target="_blank"><i class="fa fa-globe" aria-hidden="true"></i></a>
 				</li>
 			<?php endif; ?>
 		</ul>
