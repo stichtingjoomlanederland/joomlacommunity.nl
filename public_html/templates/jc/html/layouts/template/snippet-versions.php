c1c9e2b9448d5c9ce157a341993fb6838a7d40cf
Sander Potjer <github@sanderpotjer.nl>
2016-12-22 23:43:08 +0100

    Joomla version label snippet

---
+++ public_html/templates/jc/html/layouts/template/snippet-versions.php
@@ -0,0 +1,11 @@
+<?php
+defined('JPATH_BASE') or die;
+?>
+
+<?php if (in_array('joomla25', $displayData)): ?>
+    <span class="label label-joomla2"><i class="fa fa-joomla" aria-hidden="true"></i> Joomla! 2.5</span>
+<?php endif; ?>
+
+<?php if (in_array('joomla3', $displayData)) : ?>
+    <span class="label label-joomla3"><i class="fa fa-joomla" aria-hidden="true"></i> Joomla! 3</span>
+<?php endif; ?>
