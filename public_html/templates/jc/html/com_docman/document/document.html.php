b52cbfa78f7ef9df4bbc44f90a41abf1a168b199
Sander Potjer <github@sanderpotjer.nl>
2016-12-22 23:49:36 +0100

    DOCman template overrides

---
+++ public_html/templates/jc/html/com_docman/document/document.html.php
@@ -0,0 +1,262 @@
+<?
+/**
+ * @package     DOCman
+ * @copyright   Copyright (C) 2011 - 2014 Timble CVBA. (http://www.timble.net)
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.joomlatools.com
+ */
+defined('KOOWA') or die; ?>
+
+<?= helper('behavior.downloadlabel', array('params' => $params)); ?>
+
+<div class="docman_document well" itemscope itemtype="http://schema.org/CreativeWork">
+    <div class="row">
+        <div class="col-md-7">
+			<? // Header ?>
+			<? if ($params->show_document_title
+				|| ($params->show_document_recent && isRecent($document))
+				|| ($document->canPerform('edit') && $document->isLockable() && $document->isLocked())
+				|| (!$document->enabled)
+				|| (object('user')->getId() == $document->created_by)
+				|| ($params->show_document_popular && ($document->hits >= $params->hits_for_popular))
+			): ?>
+                <h2 class="koowa_header">
+					<? // Header image ?>
+					<? if ($document->icon && $params->show_document_icon): ?>
+                        <span class="koowa_header__item koowa_header__item--image_container">
+            <? if ($params->document_title_link && $link == 1): ?>
+                <a class="koowa_header__image_link <?= $params->document_title_link === 'download' ? 'docman_track_download' : ''; ?>"
+                   href="<?= ($document->title_link) ?>"
+                   data-title="<?= escape($document->title); ?>"
+                   data-id="<?= $document->id; ?>"
+		            <?= $params->download_in_blank_page && $params->document_title_link === 'download' ? 'target="_blank"' : ''; ?>><!--
+                -->
+		            <?= import('com://site/docman.document.icon.html', array(
+			            'icon'  => $document->icon,
+			            'class' => ' k-icon--size-medium' . (strlen($document->extension) ? ' k-icon-type-' . $document->extension : '')
+		            )) ?>
+            </a>
+            <? else: ?>
+	            <?= import('com://site/docman.document.icon.html', array(
+		            'icon'  => $document->icon,
+		            'class' => ' k-icon--size-medium' . (strlen($document->extension) ? ' k-icon-type-' . $document->extension : '')
+	            )) ?>
+            <? endif; ?>
+        </span>
+					<? endif ?>
+
+					<? // Header title ?>
+                    <span class="koowa_header__item">
+            <span class="koowa_wrapped_content">
+                <span class="whitespace_preserver">
+                    <? if ($params->show_document_title): ?>
+	                    <? if ($params->document_title_link && $link): ?>
+                            <a class="koowa_header__title_link <?= $params->document_title_link === 'download' ? 'docman_track_download' : ''; ?>"
+                               href="<?= ($document->title_link) ?>"
+                               data-title="<?= escape($document->title); ?>"
+                               data-id="<?= $document->id; ?>"
+			                    <?= $params->download_in_blank_page && $params->document_title_link === 'download' ? 'target="_blank"' : ''; ?>><!--
+                            --><span itemprop="name"><?= escape($document->title); ?></span></a>
+	                    <? else: ?>
+                            <span itemprop="name"><?= escape($document->title); ?></span>
+	                    <? endif; ?>
+                    <? endif; ?>
+
+	                <? // Show labels ?>
+
+	                <? // Label new ?>
+	                <? if ($params->show_document_recent && isRecent($document)): ?>
+                        <span class="label label-success"><?= translate('New'); ?></span>
+	                <? endif; ?>
+
+	                <? // Label locked ?>
+	                <? if ($document->canPerform('edit') && $document->isLockable() && $document->isLocked()): ?>
+                        <span class="label label-warning"><?= helper('grid.lock_message', array('entity' => $document)); ?></span>
+	                <? endif; ?>
+
+	                <? // Label status ?>
+	                <? if (!$document->enabled || $document->status !== 'published'): ?>
+		                <? $status = $document->enabled ? translate($document->status) : translate('Draft'); ?>
+                        &nbsp;
+                        <span class="label label-<?= $document->enabled ? $document->status : 'draft' ?>"><?= ucfirst($status); ?></span>
+	                <? endif; ?>
+
+	                <? // Label owner ?>
+	                <? if ($params->get('show_document_owner_label', 1) && object('user')->getId() == $document->created_by): ?>
+                        <span class="label label-success"><?= translate('Owner'); ?></span>
+	                <? endif; ?>
+
+	                <? // Label popular ?>
+	                <? if ($params->show_document_popular && ($document->hits >= $params->hits_for_popular)): ?>
+                        <span class="label label-danger label-important"><?= translate('Popular') ?></span>
+	                <? endif ?>
+                </span>
+            </span>
+        </span>
+                </h2>
+			<? endif; ?>
+
+
+			<? // After title - content plugin event ?>
+			<?= helper('event.trigger', array(
+				'name'       => 'onContentAfterTitle',
+				'attributes' => array($event_context, &$document, &$params, 0)
+			)); ?>
+
+
+			<? // Before display - content plugin event ?>
+			<?= helper('event.trigger', array(
+				'name'       => 'onContentBeforeDisplay',
+				'attributes' => array($event_context, &$document, &$params, 0)
+			)); ?>
+
+
+			<? // Document description ?>
+			<? if ($params->show_document_description || $params->show_document_image): ?>
+                <div class="docman_description">
+					<? if ($params->show_document_image && $document->image): ?>
+						<?= helper('behavior.thumbnail_modal'); ?>
+                        <a class="docman_thumbnail thumbnail" href="<?= $document->image_download_path ?>">
+                            <img itemprop="thumbnailUrl" src="<?= $document->image_path ?>" alt="<?= escape($document->title); ?>"/>
+                        </a>
+					<? endif ?>
+
+					<? if ($params->show_document_description):
+						$field = 'description_' . (isset($description) ? $description : 'full');
+						?>
+                        <div itemprop="description">
+							<?= prepareText($document->$field); ?>
+                        </div>
+					<? endif; ?>
+                </div>
+			<? endif ?>
+
+
+			<? // After display - content plugin event ?>
+			<?= helper('event.trigger', array(
+				'name'       => 'onContentAfterDisplay',
+				'attributes' => array($event_context, &$document, &$params, 0)
+			)); ?>
+
+
+			<? // Edit area | Import partial template from document view ?>
+			<?= import('com://site/docman.document.manage.html', array('document' => $document)) ?>
+        </div>
+        <div class="col-md-5">
+            <div class="panel panel-downloads">
+                <div class="panel-heading panel-hover">
+                    Bestandsdetails
+                </div>
+                <ul class="list-group panel-collapse in" id="joomla_3-1-5-stable-full_package-zip">
+					<? // Tags ?>
+
+					<? if ($params->show_document_tags && $document->tag_list): ?>
+                        <li class="list-group-item">
+                        <span class="info">
+	                        <?php echo JLayoutHelper::render('template.snippet-versions', explode(', ', $document->tag_list)); ?>
+                        </span>
+                            Geschikt voor
+                        </li>
+					<? endif; ?>
+
+					<? // Created ?>
+					<? if ($params->show_document_created): ?>
+                        <li class="list-group-item">
+                         <span class="info">
+            <time itemprop="datePublished" datetime="<?= $document->publish_date ?>">
+                <?= helper('date.format', array('date' => $document->publish_date)); ?>
+            </time>
+        </span>
+							<?= translate('Published on'); ?>
+                        </li>
+					<? endif; ?>
+
+					<? // Modified ?>
+					<? if ($params->show_document_modified && $document->modified_by): ?>
+                        <li class="list-group-item">
+                         <span class="info">
+            <time itemprop="dateModified" datetime="<?= $document->modified_on ?>">
+                <?= helper('date.format', array('date' => $document->modified_on)); ?>
+            </time>
+        </span>
+							<?= translate('Modified on'); ?>
+                        </li>
+					<? endif; ?>
+
+					<? // Owner ?>
+					<? if ($params->show_document_created_by && $document->created_by):
+						$owner = '<span itemprop="author">' . $document->getAuthor()->getName() . '</span>'; ?>
+                        <li class="list-group-item">
+                        <span class="info">
+                <?= translate('By {owner}', array('owner' => $owner)); ?>
+            </span>
+                            Door
+                        </li>
+					<? endif; ?>
+
+					<? // Category ?>
+					<? if ($params->show_document_category):
+						$category = '<span itemprop="genre">' . $document->category_title . '</span>'; ?>
+                        <li class="list-group-item">
+                        <span class="info">
+                <?= translate('In {category}', array('category' => $category)); ?>
+            </span>
+                            Categorie
+                        </li>
+					<? endif; ?>
+
+
+
+	                <? // Filetype and Filesize  ?>
+	                <? if (($params->show_document_size && $document->size) || ($document->storage_type == 'file' && $params->show_document_extension)): ?>
+                        <li class="list-group-item">
+                         <span class="info"><!--
+                --><? if ($document->storage_type == 'file' && $params->show_document_extension): ?><!--
+                    --><?= escape($document->extension . ($params->show_document_size && $document->size ? ', ' : '')) ?><!--
+                --><? endif ?><!--
+                --><? if ($params->show_document_size && $document->size): ?><!--
+                    --><?= helper('string.humanize_filesize', array('size' => $document->size)) ?><!--
+                --><? endif ?><!--
+                --></span>
+                        Grootte
+                    </li>
+	                <? endif; ?>
+
+
+					<? // Downloads ?>
+					<? if ($params->show_document_hits && $document->hits): ?>
+                        <li class="list-group-item">
+                            <span class="info"><?= $document->hits ?></span>
+                            <meta itemprop="interactionCount" content="UserDownloads:<?= $document->hits ?>">
+                            Downloads
+                        </li>
+					<? endif ?>
+
+                    <li class="list-group-item">
+						<? // Download area ?>
+						<? if (!object('user')->isAuthentic() || $document->canPerform('download')): ?>
+
+                            <a class="btn btn-large btn-success btn-block docman_download__button docman_track_download"
+                               href="<?= $document->download_link; ?>"
+                               data-title="<?= escape($document->title); ?>"
+                               data-id="<?= $document->id; ?>"
+								<? if (!$params->force_download): ?>
+                                    data-mimetype="<?= $document->mimetype ?>"
+                                    data-extension="<?= $document->extension ?>"
+								<? endif; ?>
+								<?= ($params->download_in_blank_page) ? 'target="_blank"' : ''; ?>>
+
+            <span class="docman_download_label">
+             <i class="fa fa-download" aria-hidden="true"></i> <?= translate('Download'); ?>
+            </span>
+                            </a>
+
+						<? endif ?>
+
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+</div>
---
+++ public_html/templates/jc/html/com_docman/list/default.html.php
@@ -0,0 +1,180 @@
+<?
+/**
+ * @package     DOCman
+ * @copyright   Copyright (C) 2011 - 2014 Timble CVBA. (http://www.timble.net)
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.joomlatools.com
+ */
+defined('KOOWA') or die; ?>
+
+<?= helper('ui.load'); ?>
+<?= helper('behavior.modal'); ?>
+
+
+<? // RSS feed ?>
+<link href="<?= route('format=rss'); ?>" rel="alternate" type="application/rss+xml" title="RSS 2.0"/>
+
+<div class="row">
+    <div class="col-md-4">
+		<?php
+		$modules = JModuleHelper::getModules('above-downloads-a');
+
+		foreach ($modules as $mod)
+		{
+			echo JModuleHelper::renderModule($mod);
+		}
+		?>
+    </div>
+    <div class="col-md-4">
+		<?php
+		$modules = JModuleHelper::getModules('above-downloads-b');
+
+		foreach ($modules as $mod)
+		{
+			echo JModuleHelper::renderModule($mod);
+		}
+		?>
+    </div>
+    <div class="col-md-4">
+		<?php
+		$modules = JModuleHelper::getModules('above-downloads-c');
+
+		foreach ($modules as $mod)
+		{
+			echo JModuleHelper::renderModule($mod);
+		}
+		?>
+    </div>
+</div>
+
+<?php if($category->title != 'New Downloads'): ?>
+
+<div class="docman_list_layout docman_list_layout--default">
+
+	<? // Page Heading ?>
+	<? if ($params->get('show_page_heading')): ?>
+        <h1 class="docman_page_heading">
+			<?= escape($params->get('page_heading')); ?>
+        </h1>
+	<? endif; ?>
+
+	<? // Toolbar ?>
+    <!--    <ktml:toolbar type="actionbar">-->
+
+	<? // Category ?>
+	<? if (($params->show_category_title && $category->title)
+		|| ($params->show_image && $category->image)
+		|| ($category->description_full && $params->show_description)
+	): ?>
+        <div class="docman_category">
+
+			<? // Header ?>
+			<? if ($params->show_category_title && $category->title): ?>
+                <h1 class="koowa_header">
+					<? // Header image ?>
+					<? if ($params->show_icon && $category->icon): ?>
+                        <span class="koowa_header__item koowa_header__item--image_container">
+                    <?= import('com://site/docman.document.icon.html', array('icon' => $category->icon, 'class' => 'k-icon--size-medium')) ?>
+                </span>
+					<? endif ?>
+
+					<? // Header title ?>
+					<? if ($params->show_category_title): ?>
+                        <span class="koowa_header__item">
+                    <span class="koowa_wrapped_content">
+                        <span class="whitespace_preserver">
+                            <?= escape($category->title); ?>
+                        </span>
+                    </span>
+                </span>
+					<? endif; ?>
+                </h1>
+			<? endif; ?>
+
+			<? // Edit area | Import partial template from category view ?>
+			<?= import('com://site/docman.category.manage.html', array('category' => $category)) ?>
+
+			<? // Category image ?>
+			<? if ($params->show_image && $category->image): ?>
+				<?= helper('behavior.thumbnail_modal'); ?>
+                <a class="docman_thumbnail thumbnail" href="<?= $category->image_path ?>">
+                    <img src="<?= $category->image_path ?>" alt="<?= escape($category->title); ?>"/>
+                </a>
+			<? endif ?>
+
+			<? // Category description full ?>
+			<? if ($category->description_full && $params->show_description): ?>
+                <div class="docman_description lead">
+					<?= prepareText($category->description_full); ?>
+                </div>
+			<? endif; ?>
+        </div>
+	<? endif; ?>
+
+
+	<? // Sub categories ?>
+	<? if ($params->show_subcategories && count($subcategories)): ?>
+		<? if ($category->id && $params->show_categories_header): ?>
+            <div class="docman_block docman_block--top_margin">
+				<? // Header ?>
+                <h3 class="koowa_header koowa_header--bottom_margin">
+					<?= translate('Categories') ?>
+                </h3>
+            </div>
+		<? endif; ?>
+
+		<? // Categories list ?>
+		<?= import('com://site/docman.list.categories.html', array(
+			'categories' => $subcategories,
+			'params'     => $params,
+			'config'     => $config
+		)) ?>
+	<? endif; ?>
+
+
+	<? // Documents header & sorting ?>
+	<? if (count($documents) || ($params->show_document_search)): ?>
+        <div class="docman_block">
+			<? if ($params->show_documents_header): ?>
+                <h3 class="koowa_header">
+					<?= translate('Documents') ?>
+                </h3>
+			<? endif; ?>
+        </div>
+
+		<? // Documents & pagination  ?>
+        <form action="" method="get" class="k-js-grid-controller">
+
+			<? // Search ?>
+			<?= import('com://site/docman.documents.search.html') ?>
+
+			<? // Sorting ?>
+			<? if ($params->show_document_sort_limit && count($documents)): ?>
+                <div class="docman_sorting form-search">
+                    <label for="sort-documents" class="control-label"><?= translate('Order by') ?></label>
+					<?= helper('paginator.sort_documents', array(
+						'sort'      => 'document_sort',
+						'direction' => 'document_direction',
+						'attribs'   => array('class' => 'input-medium', 'id' => 'sort-documents')
+					)); ?>
+                </div>
+			<? endif; ?>
+
+			<? // Document list | Import child template from documents view ?>
+			<?= import('com://site/docman.documents.list.html', array(
+				'documents' => $documents,
+				'params'    => $params
+			)) ?>
+
+			<? // Pagination  ?>
+			<? if (parameters()->total) : ?>
+				<?= helper('paginator.pagination', array(
+					'show_limit' => (bool) $params->show_document_sort_limit
+				)) ?>
+			<? endif; ?>
+
+        </form>
+	<? endif; ?>
+</div>
+
+<?php endif;?>
