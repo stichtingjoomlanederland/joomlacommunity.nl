d50869f134850661c7383b899c840b4c918d8d08
Sander Potjer <github@sanderpotjer.nl>
2017-01-01 21:30:03 +0100

    Remove extensions not in use

--- public_html/plugins/system/rsfppdf/sql/mysql/uninstall.sql
+++
@@ -1,6 +0,0 @@
-DROP TABLE IF EXISTS `#__rsform_pdfs`;
-
-DELETE FROM #__rsform_config WHERE SettingName = 'pdf.font';
-DELETE FROM #__rsform_config WHERE SettingName = 'pdf.orientation';
-DELETE FROM #__rsform_config WHERE SettingName = 'pdf.paper';
-DELETE FROM #__rsform_config WHERE SettingName = 'pdf.remote';
--- public_html/plugins/system/rsfprseventspro/sql/mysql/uninstall.sql
+++
@@ -1,4 +0,0 @@
-DROP TABLE IF EXISTS `#__rsform_rseventspro`;
-
-DELETE FROM #__rsform_component_types WHERE ComponentTypeId IN (30, 31, 32, 33, 34);
-DELETE FROM #__rsform_component_type_fields WHERE ComponentTypeId IN (30, 31, 32, 33, 34);
