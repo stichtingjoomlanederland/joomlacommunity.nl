630399c2ecc7ed639c53fb4f7ba706610c368645
Sander Potjer <github@sanderpotjer.nl>
2017-12-04 23:09:09 +0100

    Only show future events on homepage

--- public_html/templates/jc/html/mod_rseventspro_upcoming/default.php
+++ public_html/templates/jc/html/mod_rseventspro_upcoming/default.php
@@ -16,6 +16,7 @@ include_once JPATH_THEMES . '/' . $template . '/helpers/rsevents.php';
 $rsEventHelper = new ThisRSEventsProHelper();
 
 $open = !$links ? '\'target\' => \'_blank\'' : '';
+$i    = 0;
 ?>
 
 
@@ -26,20 +27,25 @@ $open = !$links ? '\'target\' => \'_blank\'' : '';
 <div class="list-group list-group-flush">
 	<?php foreach ($events as $eventid) : ?>
 		<?php
-		$details  = rseventsproHelper::details($eventid->id);
-		$category = $rsEventHelper->getCategoryName($eventid->id);
-		$category = str_replace('Joomla Gebruikersgroep', 'Gebruikersgroep', $category);
+		$details   = rseventsproHelper::details($eventid->id);
+		$category  = $rsEventHelper->getCategoryName($eventid->id);
+		$category  = str_replace('Joomla Gebruikersgroep', 'Gebruikersgroep', $category);
+		$event     = $details['event'];
+		$startdate = rseventsproHelper::date($event->start, 'Y-m-d');
+		$now       = JFactory::getDate()->format('Y-m-d');
 		?>
-		<?php if (isset($details['event']) && !empty($details['event'])) $event = $details['event'];
-		else continue; ?>
-        <a class="list-group-item" href="<?php echo rseventsproHelper::route('index.php?option=com_rseventspro&layout=show&id=' . rseventsproHelper::sef($event->id, $event->name), true, $event->itemid); ?>">
-            <div class="date-icon">
-                <span class="date-day"><?php echo rseventsproHelper::date($event->start, 'j', true); ?></span><?php echo rseventsproHelper::date($event->start, 'M', true); ?>
-            </div>
-            <h4 class="list-group-item-heading">
-				<?php echo strip_tags($event->name); ?>
-            </h4>
-            <p class="list-group-item-text"><?php echo $category; ?></p>
-        </a>
+		<?php if (isset($details['event']) && !empty($details['event']) && ($startdate >= $now) && ($i < 10)): ?>
+
+            <a class="list-group-item" href="<?php echo rseventsproHelper::route('index.php?option=com_rseventspro&layout=show&id=' . rseventsproHelper::sef($event->id, $event->name), true, $event->itemid); ?>">
+                <div class="date-icon">
+                    <span class="date-day"><?php echo rseventsproHelper::date($event->start, 'j', true); ?></span><?php echo rseventsproHelper::date($event->start, 'M', true); ?>
+                </div>
+                <h4 class="list-group-item-heading">
+					<?php echo strip_tags($event->name); ?>
+                </h4>
+                <p class="list-group-item-text"><?php echo $category; ?></p>
+            </a>
+			<?php $i++; ?>
+		<?php endif; ?>
 	<?php endforeach; ?>
 </div>
