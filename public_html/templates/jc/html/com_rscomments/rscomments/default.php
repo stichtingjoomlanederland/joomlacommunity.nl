7e692a2d8d95e8a9a3681c0c6e9097936aaa0094
Sander Potjer <github@sanderpotjer.nl>
2019-01-12 22:00:26 +0100

    #317 Update template overrides

--- public_html/templates/jc/html/com_rscomments/rscomments/default.php
+++ public_html/templates/jc/html/com_rscomments/rscomments/default.php
@@ -1,16 +1,11 @@
 <?php
 /**
- * @package       RSComments!
+ * @package RSComments!
  * @copyright (C) 2015 www.rsjoomla.com
- * @license       GPL, http://www.gnu.org/licenses/gpl-2.0.html
+ * @license GPL, http://www.gnu.org/licenses/gpl-2.0.html
  */
 
-defined('_JEXEC') or die('Restricted access');
-JText::script('COM_RSCOMMENTS_NO_SUBSCRIBER_NAME');
-JText::script('COM_RSCOMMENTS_NO_SUBSCRIBER_EMAIL');
-JText::script('COM_RSCOMMENTS_INVALID_SUBSCRIBER_EMAIL');
-JText::script('COM_RSCOMMENTS_REPORT_NO_REASON');
-JText::script('COM_RSCOMMENTS_REPORT_INVALID_CAPTCHA'); ?>
+defined('_JEXEC') or die('Restricted access'); ?>
 
 <?php if (count($this->comments)): ?>
     <h3><?php echo count($this->comments); ?><?php echo (count($this->comments) == 1) ? ' reactie' : ' reacties'; ?></h3>
@@ -19,17 +14,16 @@ JText::script('COM_RSCOMMENTS_REPORT_INVALID_CAPTCHA'); ?>
     </div>
 <?php endif; ?>
 
-<?php if ($this->pagination->get('pages.total') > 1)
-{ ?>
-    <div id="rsc_global_pagination" class="rsc_pagination pagination">
-		<?php if ($this->pagination->get('pages.current') != $this->pagination->get('pages.total'))
-		{ ?>
-            <a id="rscommentsPagination" class="rsc_button btn btn-info" href="javascript:void(0);" onclick="rsc_pagination('<?php echo($this->pagination->get('pages.current') * $this->config->nr_comments); ?>', '<?php echo $this->option; ?>', '<?php echo $this->id; ?>', '<?php echo $this->template; ?>', '<?php echo $this->override; ?>');">
+<?php if ($this->pagination->get('pagesTotal') > 1) { ?>
+	<div class="rsc_pagination pagination">
+		<?php if ($this->pagination->get('pagesCurrent') != $this->pagination->get('pagesTotal')) { ?>
+			<a class="rsc_button btn btn-info" href="javascript:void(0);" data-rsc-task="pagination" data-task-override="<?php echo $this->override; ?>">
 				<?php echo JText::_('COM_RSCOMMENTS_LOAD_MORE_COMMENTS'); ?>
-            </a>
+			</a>
 		<?php } ?>
-    </div>
-    <div id="rsc_loading_pages" style="text-align:center;display:none;">
-        <img src="<?php echo RSCommentsHelper::ImagePath('loader.gif'); ?>" alt=""/></div>
-    <span style="display:none;" id="rsc_total"><?php echo $this->total; ?></span>
+	</div>
+	<div class="rsc_loading_pages" style="text-align:center;display:none;">
+		<?php echo JHtml::image('com_rscomments/loader.gif', '', array(), true); ?>
+	</div>
+	<span style="display:none;" class="rsc_total"><?php echo $this->total; ?></span>
 <?php } ?>
