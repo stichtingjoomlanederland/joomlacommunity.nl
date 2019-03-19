2ded8e713a3e12e875bd9b794af4054f62d84e21
Hans Kuijpers <hans2103@gmail.com>
2019-03-19 11:10:01 +0100

    #267 :lipstick:

--- public_html/templates/jc/html/mod_menu/default.php
+++ public_html/templates/jc/html/mod_menu/default.php
@@ -7,100 +7,106 @@
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
+use Joomla\CMS\Helper\ModuleHelper;
+
 defined('_JEXEC') or die;
 
 // Note. It is important to remove spaces between elements.
 ?>
 <?php // The menu class is deprecated. Use nav instead. ?>
-<ul class="<?php echo $class_sfx;?>"<?php
-	$tag = '';
-	if ($params->get('tag_id') != null)
-	{
-		$tag = $params->get('tag_id').'';
-		echo ' id="'.$tag.'"';
-	}
+<ul class="<?php echo $class_sfx; ?>"<?php
+$tag = '';
+if ($params->get('tag_id') != null)
+{
+	$tag = $params->get('tag_id') . '';
+	echo ' id="' . $tag . '"';
+}
 ?>>
-<?php
-foreach ($list as $i => &$item) :
-	$class = 'item-'.$item->id;
-	if ($item->id == $active_id)
-	{
-		$class .= ' current';
-	}
+	<?php
+	foreach ($list as $i => &$item) :
+		$class = array();
 
-	if (in_array($item->id, $path))
-	{
-		$class .= ' active';
-	}
-	elseif ($item->type == 'alias')
-	{
-		$aliasToId = $item->params->get('aliasoptions');
-		if (count($path) > 0 && $aliasToId == $path[count($path) - 1])
+		$class[] = 'item-' . $item->id;
+
+		if ($item->id == $active_id)
 		{
-			$class .= ' active';
+			$class[] = 'current';
 		}
-		elseif (in_array($aliasToId, $path))
+
+		if (in_array($item->id, $path))
 		{
-			$class .= ' alias-parent-active';
+			$class[] = 'active';
 		}
-	}
+        elseif ($item->type == 'alias')
+		{
+			$aliasToId = $item->params->get('aliasoptions');
+			if (count($path) > 0 && $aliasToId == $path[count($path) - 1])
+			{
+				$class[] = 'active';
+			}
+            elseif (in_array($aliasToId, $path))
+			{
+				$class[] = 'alias-parent-active';
+			}
+		}
 
-	if ($item->type == 'separator')
-	{
-		$class .= ' divider';
-	}
+		if ($item->type == 'separator')
+		{
+			$class[] = 'divider';
+		}
 
-	if ($item->deeper)
-	{
-		$class .= ' deeper';
-	}
+		if ($item->deeper)
+		{
+			$class[] = 'deeper';
+		}
 
-	if ($item->parent)
-	{
-		$class .= ' parent';
-	}
+		if ($item->parent)
+		{
+			$class[] = 'parent';
+		}
 
-	if (!empty($class))
-	{
-		$class = ' class="'.trim($class) .'"';
-	}
+		if (!empty($class))
+		{
+			$class = ' class="' . implode(' ', $class) . '"';
+		}
 
-	echo '<li'.$class.'>';
+		echo '<li' . $class . '>';
 
-	// Render the menu item.
-	switch ($item->type) :
-		case 'separator':
-		case 'url':
-		case 'component':
-		case 'heading':
-			require JModuleHelper::getLayoutPath('mod_menu', 'default_'.$item->type);
-			break;
+		// Render the menu item.
+		switch ($item->type) :
+			case 'separator':
+			case 'url':
+			case 'component':
+			case 'heading':
+				require ModuleHelper::getLayoutPath('mod_menu', 'default_' . $item->type);
+				break;
 
-		default:
-			require JModuleHelper::getLayoutPath('mod_menu', 'default_url');
-			break;
-	endswitch;
+			default:
+				require ModuleHelper::getLayoutPath('mod_menu', 'default_url');
+				break;
+		endswitch;
 
-    // Expand button for touch devices
-    if ($item->parent)
-    {
-        echo '<span class="toggle-sub display-touch"></span>';
-    }
+		// Expand button for touch devices
+		if ($item->parent)
+		{
+			echo '<span class="toggle-sub display-touch"></span>';
+		}
 
-	// The next item is deeper.
-	if ($item->deeper)
-	{
-		echo '<ul class="nav nav-child '.$item->anchor_css.'">';
-	}
-	// The next item is shallower.
-	elseif ($item->shallower)
-	{
-		echo '</li>';
-		echo str_repeat('</ul></li>', $item->level_diff);
-	}
-	// The next item is on the same level.
-	else {
-		echo '</li>';
-	}
-endforeach;
-?></ul>
+		// The next item is deeper.
+		if ($item->deeper)
+		{
+			echo '<ul class="nav nav-child ' . $item->anchor_css . '">';
+		}
+		// The next item is shallower.
+        elseif ($item->shallower)
+		{
+			echo '</li>';
+			echo str_repeat('</ul></li>', $item->level_diff);
+		}
+		// The next item is on the same level.
+		else
+		{
+			echo '</li>';
+		}
+	endforeach;
+	?></ul>
