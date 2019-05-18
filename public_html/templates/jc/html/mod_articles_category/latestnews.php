be13c0ed7c787f1acba76d538cf3e9cc4a061bb1
Hans Kuijpers <hans2103@gmail.com>
2019-05-18 16:23:47 +0200

    #314 add image to blog list homepage for Gebruikersgroep content items when no image is provided

--- public_html/templates/jc/html/mod_articles_category/latestnews.php
+++ public_html/templates/jc/html/mod_articles_category/latestnews.php
@@ -7,6 +7,11 @@
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
+use Joomla\CMS\Factory;
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Router\Route;
+
 defined('_JEXEC') or die;
 ?>
 
@@ -15,23 +20,28 @@ defined('_JEXEC') or die;
 	<?php foreach ($list as $item) : ?>
 		<?php $images = json_decode($item->images); ?>
         <a class="list-group-item" href="<?php echo $item->link; ?>">
-			<?php if (isset($images->image_intro) && !empty($images->image_intro) and file_exists($images->image_intro)): ?>
-                <div class="news-image">
-                    <img src="<?php echo $images->image_intro; ?>"/>
-                </div>
-			<?php endif; ?>
+			<?php
+			$src = 'templates/' . Factory::getApplication()->getTemplate() . '/images/jclogo.png';
+			$alt = '';
+			if (isset($images->image_intro) && !empty($images->image_intro) and file_exists($images->image_intro)):
+				$src = $images->image_intro;
+			endif;
+			?>
+            <div class="news-image"><?php
+				echo HTMLHelper::_('image', $src, $alt);
+			?></div>
             <p class="list-group-item-meta">
-                <strong><?php echo JHtml::_('date', $item->publish_up, JText::_('j M Y')); ?></strong> door <?php echo $item->displayAuthorName; ?>
-            </p>
-            <h4 class="list-group-item-heading">
-				<?php echo $item->title; ?>
-            </h4>
-            <p class="list-group-item-text">
-				<?php echo strip_tags($item->displayIntrotext); ?>
-            </p>
+                <strong><?php echo HTMLHelper::_('date', $item->publish_up, Text::_('j M Y')); ?></strong>
+                door <?php echo $item->displayAuthorName; ?></p>
+            <h4 class="list-group-item-heading"><?php echo $item->title; ?></h4>
+            <p class="list-group-item-text"><?php echo strip_tags($item->displayIntrotext); ?></p>
         </a>
 	<?php endforeach; ?>
 </div>
-<a href="<?php echo JRoute::_('index.php?Itemid=240'); ?>" class="btn btn-nieuws btn-block">
-    Meer Joomla-nieuws
-</a>
+<?php
+    $href = Route::_('index.php?Itemid=240');
+    $text = 'Meer Joomla-nieuws';
+    $data = array(
+            'class' => 'btn btn-nieuws btn-block'
+    );
+    echo HTMLHelper::_('link', $href, $text, $data);
