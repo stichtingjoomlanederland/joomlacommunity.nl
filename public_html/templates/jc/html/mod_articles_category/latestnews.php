d5478b1644220d055013c6dcd89d1ade289d5924
Hans Kuijpers <hans2103@gmail.com>
2020-03-12 21:24:22 +0100

    #344 add label to mod_articles_category

--- public_html/templates/jc/html/mod_articles_category/latestnews.php
+++ public_html/templates/jc/html/mod_articles_category/latestnews.php
@@ -15,33 +15,44 @@ use Joomla\CMS\Router\Route;
 defined('_JEXEC') or die;
 ?>
 
-<h3><?php echo $module->title; ?></h3>
-<div class="list-group list-group-flush <?php echo $moduleclass_sfx; ?>">
-	<?php foreach ($list as $item) : ?>
-		<?php $images = json_decode($item->images); ?>
-        <a class="list-group-item" href="<?php echo $item->link; ?>">
-			<?php
-			$src = 'templates/' . Factory::getApplication()->getTemplate() . '/images/jclogo.png';
-			$alt = '';
-			if (isset($images->image_intro) && !empty($images->image_intro) and file_exists($images->image_intro)):
-				$src = $images->image_intro;
-			endif;
-			?>
-            <div class="news-image"><?php
-				echo HTMLHelper::_('image', $src, $alt);
-			?></div>
-            <p class="list-group-item-meta">
-                <strong><?php echo HTMLHelper::_('date', $item->publish_up, Text::_('j M Y')); ?></strong>
-                door <?php echo $item->displayAuthorName; ?></p>
-            <h4 class="list-group-item-heading"><?php echo $item->title; ?></h4>
-            <p class="list-group-item-text"><?php echo strip_tags($item->displayIntrotext); ?></p>
-        </a>
-	<?php endforeach; ?>
-</div>
+    <h3><?php echo $module->title; ?></h3>
+    <div class="list-group list-group-flush <?php echo $moduleclass_sfx; ?>">
+		<?php foreach ($list as $item) : ?>
+			<?php $images = json_decode($item->images); ?>
+            <a class="list-group-item" href="<?php echo $item->link; ?>">
+				<?php
+				$src = 'templates/' . Factory::getApplication()->getTemplate() . '/images/jclogo.png';
+				$alt = '';
+				if (isset($images->image_intro) && !empty($images->image_intro) and file_exists($images->image_intro)):
+					$src = $images->image_intro;
+				endif;
+				?>
+                <div class="news-image"><?php
+					echo HTMLHelper::_('image', $src, $alt);
+					?></div>
+                <p class="list-group-item-meta">
+                    <strong><?php echo HTMLHelper::_('date', $item->publish_up, Text::_('j M Y')); ?></strong>
+                    door <?php echo $item->displayAuthorName; ?>
+
+					<?php if ($item->state == 0) : ?>
+                        <span class="label label-warning"><?php echo Text::_('JUNPUBLISHED'); ?></span>
+					<?php endif; ?>
+					<?php if (strtotime($item->publish_up) > strtotime(Factory::getDate())) : ?>
+                        <span class="label label-warning"><?php echo Text::_('JNOTPUBLISHEDYET'); ?></span>
+					<?php endif; ?>
+					<?php if ((strtotime($item->publish_down) < strtotime(Factory::getDate())) && $item->publish_down != Factory::getDbo()->getNullDate()) : ?>
+                        <span class="label label-warning"><?php echo Text::_('JEXPIRED'); ?></span>
+					<?php endif; ?>
+                </p>
+                <h4 class="list-group-item-heading"><?php echo $item->title; ?></h4>
+                <p class="list-group-item-text"><?php echo strip_tags($item->displayIntrotext); ?></p>
+            </a>
+		<?php endforeach; ?>
+    </div>
 <?php
-    $href = Route::_('index.php?Itemid=240');
-    $text = 'Meer Joomla-nieuws';
-    $data = array(
-            'class' => 'btn btn-nieuws btn-block'
-    );
-    echo HTMLHelper::_('link', $href, $text, $data);
+$href = Route::_('index.php?Itemid=240');
+$text = 'Meer Joomla-nieuws';
+$data = array(
+	'class' => 'btn btn-nieuws btn-block'
+);
+echo HTMLHelper::_('link', $href, $text, $data);
